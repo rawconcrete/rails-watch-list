abadcdacc3061100c5fa6e15a85c13ac5b14b6dd
nico alavi <alavinic@gmail.com>
2025-01-11 12:51:59 +0900

    correct bookmark model based on spec.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment
-  validates :movie_id, uniqueness: { scope: list_id }
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 end
