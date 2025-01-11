9cfb53828432b90040341052139e715c367aa5ad
nico alavi <alavinic@gmail.com>
2025-01-11 12:54:23 +0900

    remove callback from movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,15 +4,4 @@ class Movie < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-
-  before_destroy :check_for_bookmarks
-
-  private
-
-  def check_for_bookmarks
-    if bookmarks.any?
-      errors.add(:base, "cannot delete films with bookmarks.")
-      throw :abort
-    end
-  end
 end
