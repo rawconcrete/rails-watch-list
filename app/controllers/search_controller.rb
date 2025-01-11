33b9e08841d0765e4b6ab17c9619a481848cf4be
nico alavi <alavinic@gmail.com>
2025-01-11 14:40:40 +0900

    complete search controller using tvmaze.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,9 @@
 class SearchController < ApplicationController
+  class SearchController < ApplicationController
+    def index
+      query = params[:query]
+      @tmdb_results = SearchService.search_tmdb(query)
+      @tvmaze_results = SearchService.search_tvmaze(query)
+    end
+  end
 end
